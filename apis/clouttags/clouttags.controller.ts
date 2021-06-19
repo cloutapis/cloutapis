bbc7e376ec0fc3ff786dd2ab03fc48f770a49c91
Ribal Al Hatem <ribalalhatem77@gmail.com>
2021-06-19 18:46:48 +0200

    getTagPosts endpoint implemented

--- apis/clouttags/clouttags.controller.ts
+++ apis/clouttags/clouttags.controller.ts
@@ -2,11 +2,12 @@ import * as express from 'express';
 import CloutTag from './clouttag.interface';
 import Post from './post.interface';
 import PostParser from './fetch/post-parser';
+import db from "../../models/index";
 
 class CloutTagsController {
   public path = '/clouttags';
   public router = express.Router();
- 
+
   private tags: CloutTag[] = [
     {
       clouttag: "Derp"
@@ -16,25 +17,25 @@ class CloutTagsController {
   private posts: Post[] = [
     {
       transactionHashHex: "1",
-      clouttags: [ {
+      clouttags: [{
         clouttag: "Derp"
       }]
     }
   ];
- 
+
   constructor() {
     this.initializeRoutes();
   }
- 
+
   public initializeRoutes() {
     this.router.get('/clouttags/trending', this.getTopTags);
     this.router.get('/clouttags/search', this.searchTags);
     this.router.get('/clouttags/fetch-test', this.testFetcher);
 
     this.router.get('/clouttag/:tag', this.getTag)
-    this.router.get('/clouttag/:tag/posts', this.getTagPosts);
+    this.router.get('/clouttag/:tag/posts/:limit/:offset', this.getTagPosts);
   }
- 
+
   getTopTags = (request: express.Request, response: express.Response) => {
     response.send(this.tags);
   }
@@ -54,18 +55,42 @@ class CloutTagsController {
     response.send(this.tags[0])
   }
 
-  getTagPosts = (request: express.Request, response: express.Response) => {
-    let { tag } = request.params;
-    let { afterTransactionHashHex } = request.query;
+  getTagPosts = async (request: express.Request, response: express.Response) => {
+    const { tag } = request.params;
+    let { limit } = request.params;
+    let { offset } = request.params;
+  
     if (!tag) {
       response.status(400).send({
         error: "Requires :tag"
-      })
+      });
     }
 
-    response.send(this.posts[0])
+    const isNumber = (value) => value != null && !isNaN(value) && !isNaN(parseFloat(value));
+    
+    let limitNum = Number(limit);
+    if(!isNumber(limitNum)){
+      limitNum = 20;
+    }
+
+    let offsetNum = Number(offset);
+    if(!isNumber(offsetNum)){
+      offsetNum = 0;
+    }
+
+    const tagLowercase = tag.toLocaleLowerCase();
+    const posts = await db.TagPost.findAll({
+      limit: limitNum,
+      offset: offsetNum,
+      where: {
+        clouttag: tagLowercase
+      },
+      order: [["postedAt", "DESC"]]
+    });
+  
+    response.send(posts);
   }
- 
+
   createAPost = (request: express.Request, response: express.Response) => {
     const { post }: { post: CloutTag } = request.body;
     this.tags.push(post);
@@ -78,5 +103,5 @@ class CloutTagsController {
     response.send(parsedPosts);
   }
 }
- 
+
 export default CloutTagsController;
