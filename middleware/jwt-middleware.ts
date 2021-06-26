d4e101ef3bce19a85b89abfe87e5fcde6ca5ec62
Chris Maddern <chris@chrismaddern.com>
2021-06-26 16:05:33 -0400

    Allow publicKey to be found in URL path

--- middleware/jwt-middleware.ts
+++ middleware/jwt-middleware.ts
@@ -12,10 +12,17 @@ class JWTAuthMiddleware {
         const { jwt: getJWT, publicKey: getPublicKey } = request.query;
         const { jwt: postJWT, publicKey: postPublicKey } = request.body;
         const jwt = getJWT || postJWT;
-        const publicKey = getPublicKey || postPublicKey;
+        let publicKey = getPublicKey || postPublicKey;
+
+        // Look for a publicKey to verify in
+        // the path if we have a JWT but no publicKey
+        if (jwt && !publicKey) {
+            publicKey = request.path.match(/[-A-Za-z0-9.]{55,56}$/)?.shift();
+        }
 
         if (!jwt || !publicKey) {
             response.status(403).send({ validJWT: false, error: "Requires jwt & publicKey"});
+            return
         }
         const isValidJWT = await this.jwtAuthenticator.authenticateJWT(jwt, publicKey);
         if (isValidJWT) {
